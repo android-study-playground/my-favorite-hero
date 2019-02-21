e548a72528b7cdeab6405a488e10737636892e57
daividvleal <daivid.v.leal@gmail.com>
2019-02-21 00:59:32 -0300

    final ignore file

--- app/src/main/kotlin/com/br/myfavoritehero/data/interfaces/HeroEventListener.kt
+++
@@ -1,8 +0,0 @@
-package com.br.myfavoritehero.data.interfaces
-
-import com.br.myfavoritehero.data.models.Hero
-
-
-interface HeroEventListener{
-    fun onHeroClicked(hero: Hero)
-}
--- app/src/main/kotlin/com/br/myfavoritehero/features/listCharacter/HeroViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.br.myfavoritehero.features.listCharacter
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class HeroViewHolder(val mLinearLayout: View): RecyclerView.ViewHolder(mLinearLayout)
