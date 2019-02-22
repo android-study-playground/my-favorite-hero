b1c1a73e881a80bcce5bbba6586984a9b09560b0
Matheus Uehara <matheuspitanco@gmail.com>
2019-02-22 16:27:42 -0300

    Adicionado bottom navigation no app

---
+++ app/src/main/kotlin/com/br/myfavoritehero/features/bottomNavigation/BottomNavigationActivity.kt
@@ -0,0 +1,14 @@
+package com.br.myfavoritehero.features.bottomNavigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.br.myfavoritehero.R
+
+class BottomNavigationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_navigation)
+    }
+
+}
