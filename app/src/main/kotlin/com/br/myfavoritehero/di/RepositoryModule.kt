a570090c3ab4d2dcf2608a92662f68e4ff7c68ff
daividvleal <daivid.v.leal@gmail.com>
2019-04-04 16:50:02 -0300

    transport to rx instead using pure retrofit

--- app/src/main/kotlin/com/br/myfavoritehero/di/RepositoryModule.kt
+++ app/src/main/kotlin/com/br/myfavoritehero/di/RepositoryModule.kt
@@ -1,10 +1,11 @@
 package com.br.myfavoritehero.di
 
 import com.br.myfavoritehero.data.request.Repository
+import com.br.myfavoritehero.data.request.RepositoryContract
 import org.koin.dsl.module.module
 
 val repositoryModule = module {
 
-    factory { Repository(get()) }
+    factory { Repository(get()) as RepositoryContract}
 
 }
