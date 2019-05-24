a5475ddce7f02ec9379341b2ea9856271075423e
Matheus Uehara <matheuspitanco@gmail.com>
2019-05-24 16:45:40 -0300

    Atualizado navigation para androidx

--- app/src/main/kotlin/com/br/myfavoritehero/features/home/HomeActivity.kt
+++ app/src/main/kotlin/com/br/myfavoritehero/features/home/HomeActivity.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.ui.setupWithNavController
-import com.br.myfavoritehero.R
 import kotlinx.android.synthetic.main.activity_bottom_navigation.navigation
+import com.br.myfavoritehero.R
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_navigation)
-        val navController: NavController = Navigation.findNavController(this, R.id.my_nav_host_fragment)
-        navigation.setupWithNavController(navController)
     }
 }
