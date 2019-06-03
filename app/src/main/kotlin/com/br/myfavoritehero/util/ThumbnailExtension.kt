f58a53307f29f755e35057859009eaf13558a7c3
Matheus Uehara <matheuspitanco@gmail.com>
2019-06-03 09:55:08 -0300

    Ajustes solicitados na PR

--- app/src/main/kotlin/com/br/myfavoritehero/util/ThumbnailExtension.kt
+++ app/src/main/kotlin/com/br/myfavoritehero/util/ThumbnailExtension.kt
@@ -4,15 +4,17 @@ import com.br.myfavoritehero.util.Constants.landscape_amazing
 import com.br.myfavoritehero.util.Constants.portrait_fantastic
 import com.br.myfavoritehero.util.Constants.separator
 import com.br.myfavoritehero.util.Constants.type
+import com.br.myfavoritehero.util.Constants.HTTPS_STRING
+import com.br.myfavoritehero.util.Constants.HTTP_STRING
 
 fun String.getLargePortraitThumbnail(): String {
-    return "${this.replace("http://", "https://")}$separator$portrait_fantastic$type"
+    return "${this.replace(HTTP_STRING, HTTPS_STRING)}$separator$portrait_fantastic$type"
 }
 
 fun String.getLargeLandscapeThumbnail(): String {
-    return "${this.replace("http://", "https://")}$separator$landscape_amazing$type"
+    return "${this.replace(HTTP_STRING, HTTPS_STRING)}$separator$landscape_amazing$type"
 }
 
 fun String.getLargeThumbnail(): String {
-    return "${this.replace("http://", "https://")}$separator$landscape_amazing$type"
+    return "${this.replace(HTTP_STRING, HTTPS_STRING)}$separator$landscape_amazing$type"
 }
