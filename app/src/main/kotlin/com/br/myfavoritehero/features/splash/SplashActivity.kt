e53157676d815be44dd4f9b4d237d9a0cf9ab8bb
daividvleal <daivid.v.leal@gmail.com>
2023-11-25 15:59:26 -0300

    fix splash screen

--- app/src/main/kotlin/com/br/myfavoritehero/features/splash/SplashActivity.kt
+++ app/src/main/kotlin/com/br/myfavoritehero/features/splash/SplashActivity.kt
@@ -2,15 +2,27 @@ package com.br.myfavoritehero.features.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.br.myfavoritehero.R
 import com.br.myfavoritehero.features.home.HomeActivity
+
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        startActivity(Intent(this, HomeActivity::class.java))
+
+        Handler().postDelayed(Runnable { /* Create an Intent that will start the Menu-Activity. */
+            val mainIntent = Intent(this, HomeActivity::class.java)
+            this@SplashActivity.startActivity(mainIntent)
+            this@SplashActivity.finish()
+        }, SPLASH_DISPLAY_LENGTH)
     }
+
+    companion object{
+        const val SPLASH_DISPLAY_LENGTH = 1000L
+    }
+
 }
